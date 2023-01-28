96e1cc26097fe263d3d93ec9d0b5839d7b01aa46
ThatCoolCoder <ThatCoolCoder@users.noreply.github.com>
2023-01-28 12:03:20 +1030

    More stuff, I don't remember what

--- Scripts/CameraZoom.cs
+++ Scripts/CameraZoom.cs
@@ -3,19 +3,19 @@ using System;
 
 public class CameraZoom : Camera
 {
-	public override void _Input(InputEvent inputEvent)
-	{
-		if (inputEvent is InputEventMouseButton mouseEvent)
-		{
+    public override void _Input(InputEvent inputEvent)
+    {
+        if (inputEvent is InputEventMouseButton mouseEvent)
+        {
 
-			if (mouseEvent.ButtonIndex == 4) // 4 = up
-			{
-				Fov -= 1;
-			}
-			if (mouseEvent.ButtonIndex == 5) // 5 = down
-			{
-				Fov += 1;
-			}
-		}
-	}
+            if (mouseEvent.ButtonIndex == 4) // 4 = up
+            {
+                Fov -= 1;
+            }
+            if (mouseEvent.ButtonIndex == 5) // 5 = down
+            {
+                Fov += 1;
+            }
+        }
+    }
 }
