a9cc77d8f40fe102978845d493fbd69f7eccd1bd
ThatCoolCoder <ThatCoolCoder@users.noreply.github.com>
2023-04-15 13:16:16 +0930

    Mess around with grass

--- Scripts/Locations/GrassScatter.cs
+++ Scripts/Locations/GrassScatter.cs
@@ -9,23 +9,39 @@ namespace Locations
         // Configure the rectangular region by scaling this.
         // Probably is a dumb way to do things but I can't think of anything better
         // Potential improvement: make it a tool script and make the mask editable in the editor.
+        // todo: Is a bit hacky, could do with some refactoring
 
+        [Export] public bool FalloffAroundCamera { get; set; } = true; // Use the camera as a central point for falloff?
+        [Export] public float FalloffMaxDistance { get; set; } = 100;
+        [Export] public float CameraMoveDistBeforeUpdate { get; set; } = 10; // Update grass falloff when 
         [Export] public int InstanceCount { get; set; } = 100;
         [Export] public Texture Mask { get; set; } // Only on white regions of this texture is grass spawned. If you leave it out then it's just everywhere
         [Export] public Texture Texture { get; set; }
         [Export] public Vector2 GrassSize { get; set; } = new Vector2(0.07f, 0.5f);
         [Export] public Vector2 GrassSizeVariation { get; set; } = Vector2.One * 0.3f; // varies by +- this amount
 
-        private const int maxMaskTries = 100; // If we fail to find a position that satisfies the mask in this many tries, give up
+        // If we fail to find a position that satisfies the mask in this many tries, give up.
+        // If grass takes too long to generate try reducing this
+        [Export] public int MaxMaskTries { get; set; } = 100;
+
+        private Vector3 size;
+        private Vector3 lastUpdatePos; // position of camera upon last update if using 
 
         public override void _Ready()
         {
+            // Clean up the editor visualisations
             GetNode<Spatial>("CSGBox").Visible = false;
 
-            var size = Scale * 2;
+            size = Scale * 2;
             GetNode<Spatial>("CSGBox").Scale = Scale;
             Scale = Vector3.One;
 
+            GenerateGrass();
+        }
+
+        private void GenerateGrass()
+        {
+            // todo: probably we could move some of this code to ready()
             Multimesh = new();
             Multimesh.TransformFormat = MultiMesh.TransformFormatEnum.Transform3d;
             var mesh = new QuadMesh();
@@ -37,12 +53,16 @@ namespace Locations
             material.FlagsTransparent = true;
             mesh.Material = material;
             material.ParamsCullMode = SpatialMaterial.CullMode.Disabled;
+            material.FlagsUnshaded = true;
 
             int trueInstanceCount = (int)(InstanceCount * SimSettings.Settings.Current.Graphics.VegetationMultiplier);
 
             Multimesh.InstanceCount = trueInstanceCount;
             var minPos = new Vector3(-size.x / 2, 0, -size.z / 2);
             var maxPos = new Vector3(size.x / 2, 0, size.z / 2);
+
+            var cameraPos = GetCameraPos();
+            lastUpdatePos = cameraPos;
 
             var maskImage = Mask == null ? null : Mask.GetData();
             if (maskImage != null) maskImage.Lock();
@@ -55,24 +75,49 @@ namespace Locations
                     (float)GD.RandRange(-GrassSizeVariation.y, GrassSizeVariation.y) + 1,
                     (float)GD.RandRange(-GrassSizeVariation.x, GrassSizeVariation.x) + 1
                 ));
-                for (int attemptNum = 0; attemptNum < maxMaskTries; attemptNum++)
+                for (int attemptNum = 0; attemptNum < MaxMaskTries; attemptNum++)
                 {
-                    var pos = VectorExtensions.Random(minPos, maxPos);
-                    var isOk = true;
+                    Vector3 pos;
+                    if (FalloffAroundCamera)
+                    {
+                        var distMultiplier = GD.Randf();
+                        var relativeToCamera = Vector3.Forward.Rotated(Vector3.Up, GD.Randf() * Mathf.Tau) * distMultiplier * FalloffMaxDistance;
+                        pos = ToLocal(relativeToCamera + cameraPos);
+
+                        if (Mathf.Abs(pos.x) > (size.x / 2) || Mathf.Abs(pos.z) > (size.z / 2)) continue;
+                    }
+                    else
+                    {
+                        pos = VectorExtensions.Random(minPos, maxPos);
+                    }
+
+                    // check if within mask
                     if (Mask != null)
                     {
                         var x = (pos.x / size.x + 0.5f) * Mask.GetWidth();
                         var y = (pos.z / size.z + 0.5f) * Mask.GetHeight();
-                        isOk = maskImage.GetPixel((int)x, (int)y).r > 0.5f;
+                        if (maskImage.GetPixel((int)x, (int)y).r < 0.5f) continue;
                     }
-                    if (isOk)
-                    {
-                        transform.origin = pos.WithY(transform.basis.Scale.y * GrassSize.y / 2);
-                        Multimesh.SetInstanceTransform(i, transform);
-                        break;
-                    }
+                    transform.origin = pos.WithY(transform.basis.Scale.y * GrassSize.y / 2);
+                    Multimesh.SetInstanceTransform(i, transform);
+                    break;
                 }
             };
+        }
+
+        private Vector3 GetCameraPos()
+        {
+            return GetViewport().GetCamera().GlobalTranslation;
+        }
+
+        public override void _Process(float delta)
+        {
+            if (Engine.GetFramesDrawn() % 30 == 0 &&
+                FalloffAroundCamera &&
+                GetCameraPos().DistanceSquaredTo(lastUpdatePos) > CameraMoveDistBeforeUpdate * CameraMoveDistBeforeUpdate)
+            {
+                GenerateGrass();
+            }
         }
     }
 }
