bda1aa3f42e53ec9dc618b1ca3b77b985a7a03a9
That-Cool-Coder <62006241+That-Cool-Coder@users.noreply.github.com>
2023-05-28 09:32:21 +0930

    Experiment with better pine

--- Art/Common/VegetationRender.py
+++ Art/Common/VegetationRender.py
@@ -4,6 +4,8 @@ import bpy
 import os
 
 # Render the item at a bunch of angles (configurable) and save it
+# Usage: Parent your camera to an object called CameraHolder that is located in the center of your vegetation
+
 # Todo: bake normals for each angle too
 
 num_steps = 3
@@ -20,6 +22,5 @@ for i in range(num_steps):
     angle_int = int(round(math.degrees(angle)))
     bpy.context.scene.render.filepath = os.path.join(directory, f'{name}{angle_int}.png')
     bpy.ops.render.render(write_still = True)
-
 
 holder.rotation_euler[2] = math.radians(0)
