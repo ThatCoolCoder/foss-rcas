6b74a9279516f0d07dc4185786adbd0447f77562
ThatCoolCoder <ThatCoolCoder@users.noreply.github.com>
2023-11-10 20:26:02 +1030

    experiment with base flow in wind

--- Scripts/Physics/Fluids/Air.cs
+++ Scripts/Physics/Fluids/Air.cs
@@ -13,18 +13,30 @@ public partial class Air : Node3D, ISpatialFluid
 
     public WindSettings WindSettings { get; set; } = WindSettings.SlightGusts;
 
+    private FastNoiseLite mainNoise = new();
     private FastNoiseLite turbulenceNoise = new(); // why we need 3 noise when we can just use 1 and offset sample?
     private float time = 0;
+
+    private Vector3 cachedMainFlow;
 
     public override void _Ready()
     {
         turbulenceNoise.NoiseType = FastNoiseLite.NoiseTypeEnum.Value;
         turbulenceNoise.FractalOctaves = 1;
+
+        mainNoise.NoiseType = FastNoiseLite.NoiseTypeEnum.Value;
+        mainNoise.FractalOctaves = 2;
     }
 
     public override void _PhysicsProcess(double delta)
     {
         time += (float)delta;
+
+        var mainFlowDirectionDegrees = WindSettings.DirectionDegrees;
+        mainFlowDirectionDegrees += mainNoise.GetNoise1D(time * 10 * WindSettings.DirectionChangeFrequency) * WindSettings.DirectionVariabilityDegrees;
+        var mainFlowDirection = Mathf.DegToRad(mainFlowDirectionDegrees);
+        var mainFlowSpeed = WindSettings.Speed + Utils.MapNumber(mainNoise.GetNoise1D(time * 10 * WindSettings.GustFrequency + 2502.2f), -1, 1, 0, WindSettings.GustSpeedDelta);
+        cachedMainFlow = Vector3.Right.Rotated(Vector3.Up, mainFlowDirection) * mainFlowSpeed;
     }
 
     public override void _Process(double delta)
@@ -44,18 +56,16 @@ public partial class Air : Node3D, ISpatialFluid
 
     public Vector3 VelocityAtPoint(Vector3 point)
     {
-        // todo: add base flow as well as flow field
-        var flow = Vector3.Zero;
+        var flow = cachedMainFlow;
 
-        // Define these for setting sensible base values for the frequencies to be based on
-        var baseTime = time * 100f;
+        // Define this for setting sensible base values for the frequencies to be based on
         var basePos = point * 10f;
 
-        var turbulenceSpeed = Utils.MapNumber(turbulenceNoise.GetNoise3D(basePos.X, basePos.Y, baseTime * WindSettings.TurbulenceFrequency), -1, 1, 0, WindSettings.TurbulenceMaxSpeed);
+        var turbulenceSpeed = Utils.MapNumber(turbulenceNoise.GetNoise3D(basePos.X, basePos.Y, time * 100 * WindSettings.TurbulenceFrequency), -1, 1, 0, WindSettings.TurbulenceMaxSpeed);
         // Mapping direction from 0 to 4 rotations in an effort to make it not biased to center
-        var turbulenceDirection = Utils.MapNumber(turbulenceNoise.GetNoise3D(basePos.X, basePos.Y + 1000, baseTime * .5f * WindSettings.TurbulenceFrequency), -1, 1, 0, Mathf.Pi * 2 * 4);
+        var turbulenceDirection = Utils.MapNumber(turbulenceNoise.GetNoise3D(basePos.X, basePos.Y + 1000, time * 5 * WindSettings.TurbulenceFrequency), -1, 1, 0, Mathf.Pi * 2 * 4);
 
-        var windshearSpeed = Utils.MapNumber(turbulenceNoise.GetNoise3D(basePos.X, basePos.Y + 1100, baseTime * WindSettings.TurbulenceFrequency), -1, 1,
+        var windshearSpeed = Utils.MapNumber(turbulenceNoise.GetNoise3D(basePos.X, basePos.Y + 1100, time * 100 * WindSettings.TurbulenceFrequency), -1, 1,
             -WindSettings.WindshearMaxSpeed, WindSettings.WindshearMaxSpeed);
 
         flow += new Vector3(Mathf.Cos(turbulenceDirection) * turbulenceSpeed, windshearSpeed, Mathf.Sin(turbulenceDirection) * turbulenceSpeed);
